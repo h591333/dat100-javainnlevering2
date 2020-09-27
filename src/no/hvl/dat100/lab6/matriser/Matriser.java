1cd62be30f92ddf1f0eb157f8802e5da2b1c52c6
augus <augus@DESKTOP-EUP9QV9>
2020-09-27 23:12:24 +0200

    matriser

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -1,50 +1,104 @@
 package no.hvl.dat100.lab6.matriser;
 
 public class Matriser {
-
+	public static void main (String[] args) {
+		int [][] matrise = {{1,2,3},{4,5,6},{7,8,9}};
+		System.out.println(tilStreng(matrise));
+		
+	}
+		
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		for (int[] test : matrise) {
+			System.out.println();
+			 for (int i : test) {
+			System.out.print(i + " ");
+				
+			 }
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
+		String utskrift = "";
+        for(int[] rad : matrise) {
+        	System.out.println();
+        	for(int pos : rad) {
+        	System.out.print(pos + " ");
+        	
+        	}
+        }
+        utskrift += ("");
+		return utskrift;
+		
 		
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
-	
+		int[][] nyMat = new int[matrise.length][matrise[0].length];
+		
+		for(int i = 0; i < nyMat.length; i++) {
+			
+			for (int j = 0; j < nyMat[i].length; j++) {
+				
+				nyMat[i][j] = matrise[i][j] * tall;
+				
+			}
+		}
+		
+		return nyMat;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
+		
+		int rad1, kollone1, rad2, kollone2;
+		
+		boolean flag = true;
+	
+		int tabell[][] = {
+			{1, 2, 3},
+			{4, 5, 6},
+			{7, 8, 9}
+			
+		};
+		
+		rad1 = a.length;
+		kollone1 = a[0].length;
+		
+		rad2 = b.length;
+		kollone2 = b[0].length;
+		
+		if (rad1 != rad2 || kollone1 != kollone2) {
+			System.out.println("Matrisene er ikke like");
+		
+		} else {
+			
+			for (int i = 0; i < rad1; i++) {
+				for (int j = 0; j < kollone1; j++) {
+					if (a[i][j] != b[i][j]) {
+						flag = false;
+						break;
+					}
+				}
+			}
+			
+			if (flag)
+				System.out.println("Matrisene er like");
+			else
+				System.out.println("Matrisene er ikke like");
+		}
+			
+		return flag;
 
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
+		
 	}
 	
-	// e)
-	public static int[][] speile(int[][] matrise) {
+	// e) ikke gjort
+	//public static int[][] speile(int[][] matrise) {
+	// f) ikke gjort
+	//public static int[][] multipliser(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("speile ikke implementert");
-	
-	}
-
-	// f)
-	public static int[][] multipliser(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("multipliser ikke implementert");
-	
-	}
 }
